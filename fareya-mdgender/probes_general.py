de38449c837c79ae31b97f55ae1e30e7e7aa4ed4
Linus Jen <ljen31@massmutual.com>
2023-05-17 12:36:39 -0400

    Add Fareya's code

---
+++ fareya-mdgender/probes_general.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+
+class SimpleProbe(nn.Module):
+    def __init__(self, in_dim, out_dim=3):
+        super(SimpleProbe, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(in_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, out_dim),
+        )
+
+    def forward(self, x):
+        return self.model(x)
